761566be48a6800a5e38cf010c9b08c427d78e3b
Your Name <you@example.com>
2025-10-10 02:00:27 +0200

    Fix image paths

--- src/assets/components/ProductUses.jsx
+++ src/assets/components/ProductUses.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import FaceCreams from '../img/facecream.png'
+import FaceCreams from '../img/faceCream.png'
 import handCream from '../img/handCream.png'
 import tattoCream from '../img/tattoCream.png'
 import bottle from '../img/botella1.png'
